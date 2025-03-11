a2cd48ad0c70888306bfd886f90ee6f62871a35a
이재백 <dbswlrla112@naver.com>
2025-03-11 15:49:36 +0900

    [S12P21A701-285] chore: Add electron settings in project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  build: {
+    outDir: "dist", // 빌드 결과물이 생성될 폴더
+  },
+});
