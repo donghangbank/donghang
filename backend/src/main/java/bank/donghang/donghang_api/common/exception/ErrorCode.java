ff295e6caac38e9ea1344ea25abc8af956ce9976
박종하 <jonghaparkdevelop@gmail.com>
2025-03-11 13:38:13 +0900

    [S12P21A701-248] chore: Create BadRequestException

---
+++ backend/src/main/java/bank/donghang/donghang_api/common/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package bank.donghang.donghang_api.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    INVALID_REQUEST(1000, "유효하지 않은 요청입니다.")
+    ;
+
+    private final int code;
+    private final String message;
+}
