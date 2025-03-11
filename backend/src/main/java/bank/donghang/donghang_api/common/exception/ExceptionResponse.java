9281c62d823cc439385bf0ccbcd677d93da90fe4
박종하 <jonghaparkdevelop@gmail.com>
2025-03-11 13:41:53 +0900

    [S12P21A701-248] chore: Create ExceptionResponse

---
+++ backend/src/main/java/bank/donghang/donghang_api/common/exception/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package bank.donghang.donghang_api.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ExceptionResponse {
+
+    private final int code;
+    private final String message;
+}
