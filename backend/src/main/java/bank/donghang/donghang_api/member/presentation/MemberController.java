4953ac16d2b9ace29802e92bbc4b721d24594303
박종하 <jonghaparkdevelop@gmail.com>
2025-03-12 10:17:19 +0900

    [S12P21A701-244] feature: Create MemberController

---
+++ backend/src/main/java/bank/donghang/donghang_api/member/presentation/MemberController.java
@@ -0,0 +1,23 @@
+package bank.donghang.donghang_api.member.presentation;
+
+import bank.donghang.donghang_api.member.application.MemberService;
+import bank.donghang.donghang_api.member.dto.response.MemberDetailResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/{memeberId}")
+    public ResponseEntity<MemberDetailResponse> getUserInformation(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.findMember(memberId));
+    }
+}
