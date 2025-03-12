7bd1a1c9d3c3b55349f52737f98000033c9a69ec
박종하 <jonghaparkdevelop@gmail.com>
2025-03-12 17:57:26 +0900

    [S12P21A701-235] fix: Add JpaAudition configuration

---
+++ backend/src/main/java/bank/donghang/donghang_api/config/JpaAuditionConfig.java
@@ -0,0 +1,9 @@
+package bank.donghang.donghang_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditionConfig {
+}
