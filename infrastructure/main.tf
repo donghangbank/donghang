9b8fae47f6a46e910479d1e15a86d1b4349c41cd
한종우 <jongwooo.han@gmail.com>
2025-03-13 11:01:56 +0900

    [S12P21A701-217] chore: Create vpc

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,8 @@
+module "networing" {
+  source   = "./Modules/Networking"
+  vpc_cidr = var.vpc_cidr
+}
+
 module "s3" {
   source = "./Modules/S3"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,27 @@
+variable "availability_zones" {
+  type = list(string)
+}
+
 variable "aws_profile" {
   type = string
 }
 
 variable "aws_region" {
+  type = string
+}
+
+variable "database_subnet_cidr_block" {
+  type = list(string)
+}
+
+variable "private_subnet_cidr_block" {
+  type = list(string)
+}
+
+variable "public_subnet_cidr_block" {
+  type = list(string)
+}
+
+variable "vpc_cidr" {
   type = string
 }
