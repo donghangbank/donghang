03a7d23d9324c728bc629d710ba0aa19e7353c4f
박종하 <jonghaparkdevelop@gmail.com>
2025-03-13 09:29:48 +0900

    [S12P21A701-328] fix: Add missing annotation

--- backend/src/main/java/bank/donghang/donghang_api/cardcompany/application/CardCompanyService.java
+++ backend/src/main/java/bank/donghang/donghang_api/cardcompany/application/CardCompanyService.java
@@ -3,6 +3,7 @@ package bank.donghang.donghang_api.cardcompany.application;
 import bank.donghang.donghang_api.cardcompany.domain.CardCompany;
 import bank.donghang.donghang_api.cardcompany.domain.repository.CardCompanyRepository;
 import bank.donghang.donghang_api.cardcompany.dto.request.CardCompanyCreateRequest;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ public class CardCompanyService {
 
     private final CardCompanyRepository cardCompanyRepository;
 
+    @Transactional
     public Long createCardCompany(CardCompanyCreateRequest request) {
 
         CardCompany cardCompany = CardCompany.createCardCompany(
