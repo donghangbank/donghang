b2ec7edf907a9dca6583a9c61556480a3c45640c
한종우 <jongwooo.han@gmail.com>
2025-03-13 10:37:36 +0900

    [S12P21A701-222] chore: Add s3 bucket for dev workspace

---
+++ infrastructure/Modules/S3/main.tf
@@ -0,0 +1,40 @@
+resource "aws_s3_bucket" "asset_bucket" {
+  bucket = "donghang-bucket-${terraform.workspace}"
+
+  tags = {
+    Name = "donghang-bucket-${terraform.workspace}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "asset_public_access" {
+  bucket = aws_s3_bucket.asset_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "asset_bucket_policy" {
+  bucket = aws_s3_bucket.asset_bucket.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.asset_public_access
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowPublicReadWriteAccess"
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.asset_bucket.arn}/*"
+      }
+    ]
+  })
+}
