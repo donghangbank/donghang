c2b15ec6f1c07eda92780104ab2d697bcaa065d9
한종우 <jongwooo.han@gmail.com>
2025-03-17 13:06:27 +0900

    [S12P21A701-445] style: Update main.tf to format code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,15 +13,15 @@ module "networing" {
   vpc_cidr                   = var.vpc_cidr
 }
 
+module "s3" {
+  source     = "./Modules/S3"
+  vpce_s3_id = module.networing.vpce_s3_id
+}
+
 module "security_group" {
   source                     = "./Modules/SecurityGroup"
   database_subnet_cidr_block = var.database_subnet_cidr_block
   private_subnet_cidr_block  = var.private_subnet_cidr_block
   public_subnet_cidr_block   = var.public_subnet_cidr_block
   vpc_id                     = module.networing.vpc_id
-}
-
-module "s3" {
-  source     = "./Modules/S3"
-  vpce_s3_id = module.networing.vpce_s3_id
 }
