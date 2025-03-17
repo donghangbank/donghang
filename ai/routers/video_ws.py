aba54a510609109c5f471dba8603f91d034073d3
조강민 <jkm96529@naver.com>
2025-03-17 12:45:39 +0900

    [S12P21A701-411] feature: Create age prediction websocket function

---
+++ ai/routers/video_ws.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+import json
+import logging
+from utils.image_processing import *
+from utils.age_prediction_utils import *
+
+logger = logging.getLogger("logger")
+
+router = APIRouter()
+
+@router.websocket("/ws/video")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    logger.info("웹소켓 연결")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message = json.loads(data)
+
+            if 'image' not in message:
+                logger.error("이미지가 전송되지 않았습니다.")
+                continue
+
+            img = decode_image(message['image'])
+            if img is None:
+                logger.error("이미지 디코딩에 실패했습니다.")
+                continue
+
+            predicted_age = predict_age(img)
+
+            result = {"predicted_age": predicted_age}
+            await websocket.send_text(json.dumps(result))
+    except WebSocketDisconnect:
+        logger.info("웹소켓 연결 종료")
