0e3423ca6c0eadcb34c171dbeeedd44280548cdf
한종우 <jongwooo.han@gmail.com>
2025-03-18 16:58:26 +0900

    [S12P21A701-489] fix: Comment out the contextLoads method

--- backend/src/test/java/bank/donghang/core/DonghangApiApplicationTests.java
+++ backend/src/test/java/bank/donghang/core/DonghangApiApplicationTests.java
@@ -1,13 +1,12 @@
 package bank.donghang.core;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = DonghangApiApplication.class)
 class DonghangApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
