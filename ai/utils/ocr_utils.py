6ce0f1360b88546ed525ea5a87eca9c12c18757e
조강민 <jkm96529@naver.com>
2025-03-19 12:58:58 +0900

    [S12P21A701-416] feature: Configure ocr reader

--- ai/utils/ocr_utils.py
+++ ai/utils/ocr_utils.py
@@ -3,8 +3,16 @@ import numpy as np
 from PIL import Image
 import io
 import logging
+import torch
+import easyocr
+
 
 logger = logging.getLogger("logger")
+
+gpu_available = torch.cuda.is_available()
+reader = easyocr.Reader(['ko'], gpu=gpu_available)
+
+EXCLUDED_KEYWORDS = ["주민등록증", "(재외국민)"]
 
 async def decode_image(file: UploadFile) -> np.ndarray:
     """이미지를 OCR에 사용할 수 있도록 변환"""
