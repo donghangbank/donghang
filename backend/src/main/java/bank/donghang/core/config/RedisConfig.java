b05eda7bd18d3032490397651f48af410b1659ea
박종하 <jonghaparkdevelop@gmail.com>
2025-03-19 16:12:00 +0900

    [S12P21A701-476] refactor: Unite RedissonConfig and RedisConfig

--- backend/src/main/java/bank/donghang/core/config/RedisConfig.java
+++ backend/src/main/java/bank/donghang/core/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package bank.donghang.core.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,8 @@ import lombok.RequiredArgsConstructor;
 @EnableRedisRepositories
 @RequiredArgsConstructor
 public class RedisConfig {
+
+	private static final String REDISSON_HOST_PREFIX = "redis://";
 
 	private final ObjectMapper objectMapper;
 
@@ -40,5 +45,14 @@ public class RedisConfig {
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, Object.class));
 		return redisTemplate;
+	}
+
+	@Bean
+	public RedissonClient redissonClient() {
+		RedissonClient redisson = null;
+		Config config = new Config();
+		config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort);
+		redisson = Redisson.create(config);
+		return redisson;
 	}
 }
