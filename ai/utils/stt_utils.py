391e197c64259495a3e816ba2dbdbf47227693d5
조강민 <jkm96529@naver.com>
2025-03-20 11:24:48 +0900

    [S12P21A701-423] feature: Implement stt function

--- ai/utils/stt_utils.py
+++ ai/utils/stt_utils.py
@@ -1,6 +1,7 @@
 import logging
 import torch
 from faster_whisper import WhisperModel
+import io
 
 logger = logging.getLogger("logger")
 
@@ -12,3 +13,14 @@ stt_model = WhisperModel(
     device=device,
     compute_type="int8"
 )
+
+async def stt(audio_data: bytes):
+    """ STT 변환 """
+    try:
+        audio_stream = io.BytesIO(audio_data)
+        segments, _ = stt_model.transcribe(audio_stream, language="ko", vad_filter=True)
+        return " ".join([segment.text for segment in segments])
+    except Exception as e:
+        logger.error(f"STT 변환 오류: {e}")
+        return ""
+
