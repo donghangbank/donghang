ac5824eb9df0323da0228ea186329243ae32901b
이주현 <cmh1199@naver.com>
2025-03-21 00:19:20 +0900

    [S12P21A701-547] style: apply coding conventions and format fixes

--- backend/src/main/java/bank/donghang/core/account/dto/response/AccountRegisterResponse.java
+++ backend/src/main/java/bank/donghang/core/account/dto/response/AccountRegisterResponse.java
@@ -6,24 +6,24 @@ import bank.donghang.core.account.domain.Account;
 import bank.donghang.core.accountproduct.domain.AccountProduct;
 
 public record AccountRegisterResponse(
-	String productName,
-	String withdrawalAccountNumber,
-	String payoutAccountNumber,
-	String accountNumber,
-	Long accountBalance,
-	Double interestDate,
-	Date accountExpiryDate
+        String productName,
+        String withdrawalAccountNumber,
+        String payoutAccountNumber,
+        String accountNumber,
+        Long accountBalance,
+        Double interestDate,
+        Date accountExpiryDate
 ) {
-	public static AccountRegisterResponse from(Account account, AccountProduct product, String withdrawalAccountNumber,
-		String payoutAccountNumber) {
-		return new AccountRegisterResponse(
-			product.getAccountProductName(),
-			withdrawalAccountNumber,
-			payoutAccountNumber,
-			account.getAccountTypeCode() + account.getBranchCode() + account.getAccountNumber(),
-			account.getAccountBalance(),
-			product.getInterestRate(),
-			account.getAccountExpiryDate()
-		);
-	}
+    public static AccountRegisterResponse from(Account account, AccountProduct product, String withdrawalAccountNumber,
+                                               String payoutAccountNumber) {
+        return new AccountRegisterResponse(
+                product.getAccountProductName(),
+                withdrawalAccountNumber,
+                payoutAccountNumber,
+                account.getAccountTypeCode() + account.getBranchCode() + account.getAccountNumber(),
+                account.getAccountBalance(),
+                product.getInterestRate(),
+                account.getAccountExpiryDate()
+        );
+    }
 }
