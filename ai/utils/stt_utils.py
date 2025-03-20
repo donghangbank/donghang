1908f941237153b9d08f6023a20c5924d4e6c909
조강민 <jkm96529@naver.com>
2025-03-20 11:23:42 +0900

    [S12P21A701-420] feature: Config stt model

--- ai/utils/stt_utils.py
+++ ai/utils/stt_utils.py
@@ -1,7 +1,14 @@
 import logging
 import torch
+from faster_whisper import WhisperModel
 
 logger = logging.getLogger("logger")
 
 gpu_available = torch.cuda.is_available()
 device = "cuda" if gpu_available else "cpu"
+
+stt_model = WhisperModel(
+    "small",
+    device=device,
+    compute_type="int8"
+)
