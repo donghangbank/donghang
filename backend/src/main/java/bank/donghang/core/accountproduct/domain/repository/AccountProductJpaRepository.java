0f52540be86e10c529c326fb60ce0b8d78fad566
이주현 <cmh1199@naver.com>
2025-03-21 09:49:54 +0900

    [S12P21A701-547] style: fix checkstyle issues

--- backend/src/main/java/bank/donghang/core/accountproduct/domain/repository/AccountProductJpaRepository.java
+++ backend/src/main/java/bank/donghang/core/accountproduct/domain/repository/AccountProductJpaRepository.java
@@ -10,5 +10,6 @@ import bank.donghang.core.accountproduct.domain.AccountProduct;
 @Repository
 public interface AccountProductJpaRepository extends JpaRepository<AccountProduct, Long> {
 	AccountProduct findAccountProductByAccountProductId(Long productId);
+
 	boolean existsAccountProductByAccountProductId(Long productId);
 }
