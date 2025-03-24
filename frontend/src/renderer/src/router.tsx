01bc222209a3bdf2c00adaa64371ba5dbe9dec05
이재백 <dbswlrla112@naver.com>
2025-03-24 16:42:37 +0900

    [S12P21A701-610] fix: Fix route path

--- frontend/src/renderer/src/router.tsx
+++ frontend/src/renderer/src/router.tsx
@@ -1,14 +1,10 @@
 import { createHashRouter } from "react-router-dom";
-import App from "./pages/App";
-import DepositPage from "./pages/deposit/DepositPage";
+import App from "./App";
 
-// createHashRouter를 사용해 라우트를 설정
 const router = createHashRouter([
 	{
 		path: "/",
-		children: [
-			{ index: true, element: <App /> },
-		]
+		children: [{ index: true, element: <App /> }]
 	}
 ]);
 
