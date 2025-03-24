7f26ba5fd8b11c08dd45d5dfaf0012bb467c2438
이재백 <dbswlrla112@naver.com>
2025-03-24 16:00:11 +0900

    [S12P21A701-602] feature: Add rgb, background custom colors in tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				red: "#FD2F2F",
+				green: "#22B16E",
+				blue: "#3D5EDC",
+				background: "#F1F2F5"
+			}
+		}
 	},
 	plugins: []
 };
