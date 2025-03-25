73f7a56bced1f867d70ee88b12da348f813660b8
이재백 <dbswlrla112@naver.com>
2025-03-25 17:47:28 +0900

    [S12P21A701-657] feature: Implements AudioVisualizerStatic Component

---
+++ frontend/src/renderer/src/components/common/AudioVisualizerStatic.tsx
@@ -0,0 +1,12 @@
+const AudioVisualizerStatic: React.FC = () => {
+	return (
+		<svg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
+			<title>Static AudioVisualizer</title>
+			<rect x="5" y="20" width="10" height="20" fill="#fff" />
+			<rect x="25" y="10" width="10" height="40" fill="#fff" />
+			<rect x="45" y="30" width="10" height="10" fill="#fff" />
+		</svg>
+	);
+};
+
+export default AudioVisualizerStatic;
