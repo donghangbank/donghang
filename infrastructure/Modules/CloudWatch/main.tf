cf91dd217696ed7bac8cff8cdd0e653ce1299d96
한종우 <jongwooo.han@gmail.com>
2025-03-25 11:13:12 +0900

    [S12P21A701-576] chore: Create webserver log group

---
+++ infrastructure/Modules/CloudWatch/main.tf
@@ -0,0 +1,8 @@
+resource "aws_cloudwatch_log_group" "webserver_log_group" {
+  name              = "/ecs/donghang-webserver-service"
+  retention_in_days = 7
+
+  tags = {
+    Name = "donghang-webserver-log-group"
+  }
+}
---
+++ infrastructure/Modules/CloudWatch/outputs.tf
@@ -0,0 +1,3 @@
+output "webserver_log_group_name" {
+  value = aws_cloudwatch_log_group.webserver_log_group.name
+}
