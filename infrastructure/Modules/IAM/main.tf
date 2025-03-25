add0b9a9a9acbcdb491952abb7311fc672e15198
한종우 <jongwooo.han@gmail.com>
2025-03-25 13:08:24 +0900

    [S12P21A701-633] chore: Add ecs service role

--- infrastructure/Modules/IAM/main.tf
+++ infrastructure/Modules/IAM/main.tf
@@ -60,6 +60,51 @@ resource "aws_iam_instance_profile" "ecs_instance_profile" {
   role = aws_iam_role.ecs_instance_role.name
 }
 
+data "aws_iam_policy_document" "ecs_service_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs.amazonaws.com", ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "ecs_service_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:Describe*",
+      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+      "elasticloadbalancing:DeregisterTargets",
+      "elasticloadbalancing:Describe*",
+      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+      "elasticloadbalancing:RegisterTargets",
+      "ec2:DescribeTags"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "ecs_service_role" {
+  name               = "donghang-ecs-service-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_service_policy.json
+}
+
+resource "aws_iam_role_policy" "ecs_service_role_policy" {
+  name   = "donghang-ecs-service-role-policy"
+  role   = aws_iam_role.ecs_service_role.id
+  policy = data.aws_iam_policy_document.ecs_service_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service_role_attachment" {
+  role       = aws_iam_role.ecs_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
 data "aws_iam_policy_document" "task_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
