fab20453f75e6a7993984b638484929c53f2f74f
한종우 <jongwooo.han@gmail.com>
2025-03-25 11:14:07 +0900

    [S12P21A701-577] chore: Create appserver log group

--- infrastructure/Modules/CloudWatch/main.tf
+++ infrastructure/Modules/CloudWatch/main.tf
@@ -6,3 +6,12 @@ resource "aws_cloudwatch_log_group" "webserver_log_group" {
     Name = "donghang-webserver-log-group"
   }
 }
+
+resource "aws_cloudwatch_log_group" "appserver_log_group" {
+  name              = "/ecs/donghang-appserver-service"
+  retention_in_days = 7
+
+  tags = {
+    Name = "donghang-appserver-log-group"
+  }
+}
