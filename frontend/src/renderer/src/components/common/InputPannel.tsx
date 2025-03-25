b54f4d1878ae93f80ac217ac638c1cae5da6259c
이재백 <dbswlrla112@naver.com>
2025-03-25 12:30:25 +0900

    [S12P21A701-627] feature: Implements InputPannel Component

---
+++ frontend/src/renderer/src/components/common/InputPannel.tsx
@@ -0,0 +1,57 @@
+import PropTypes from "prop-types";
+
+interface InputPanelProps {
+	inputValue: string;
+	mainLabel: string;
+	subLabel: string;
+	format: (value: string) => string;
+}
+
+export const InputPanel: React.FC<InputPanelProps> = ({
+	inputValue,
+	mainLabel,
+	subLabel,
+	format
+}) => {
+	return (
+		<div className="flex flex-col h-screen">
+			<div className="flex flex-col flex-1 justify-between m-10 p-10 bg-white rounded-2xl">
+				<div className="text-right flex flex-col justify-center flex-1">
+					<span className="text-8xl leading-snug">
+						{mainLabel}를 입력하시고
+						<br />
+						<span className="text-green">확인</span>을 눌러주십시오
+						{(mainLabel === "비밀번호" || mainLabel === "주민등록번호") && (
+							<>
+								<br />
+								<span className="text-6xl text-red">* 입력하실 때 주의하세요!</span>
+							</>
+						)}
+					</span>
+				</div>
+				<div className="flex flex-col p-10 gap-16 bg-background rounded-2xl">
+					<div className="flex justify-between">
+						<span className="text-8xl text-blue">{subLabel}</span>
+						<span className="text-8xl text-blue">1:00</span>
+					</div>
+					<div className="flex flex-col">
+						<input
+							className="flex-1 text-8xl rounded-2xl border-2 border-black text-right px-2.5"
+							value={format(inputValue)}
+							readOnly
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+InputPanel.propTypes = {
+	inputValue: PropTypes.string.isRequired,
+	mainLabel: PropTypes.string.isRequired,
+	subLabel: PropTypes.string.isRequired,
+	format: PropTypes.func.isRequired
+};
+
+export default InputPanel;
--- frontend/src/renderer/src/pages/deposit/DepositPage.tsx
+++ frontend/src/renderer/src/pages/deposit/DepositPage.tsx
@@ -1,9 +1,12 @@
+import InputPanel from "@renderer/components/common/InputPannel";
 import NumberPad from "@renderer/components/common/NumberPad";
 
 export const DepositPage: React.FC = () => {
 	return (
 		<div className="flex h-screen">
-			<div style={{ width: "66.67vw" }}>왼쪽 영역</div>
+			<div style={{ width: "66.67vw" }}>
+				<InputPanel
+				/>
 			<div style={{ width: "33.33vw" }}>
 				<NumberPad />
 			</div>
