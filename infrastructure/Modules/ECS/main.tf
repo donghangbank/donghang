6dcc007e1331611182c36991ee8cd58461df2e7c
한종우 <jongwooo.han@gmail.com>
2025-03-25 11:01:46 +0900

    [S12P21A701-565] chore: Create appserver ecs cluster

--- infrastructure/Modules/ECS/main.tf
+++ infrastructure/Modules/ECS/main.tf
@@ -10,3 +10,16 @@ resource "aws_ecs_cluster" "webserver_ecs_cluster" {
     Name = "donghang-webserver-ecs-cluster"
   }
 }
+
+resource "aws_ecs_cluster" "appserver_ecs_cluster" {
+  name = "donghang-appserver-ecs-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name = "donghang-appserver-ecs-cluster"
+  }
+}
