5188b32e9ed329e61d0563c3909aaa0b2cb2097b
이재백 <dbswlrla112@naver.com>
2025-03-25 17:46:49 +0900

    [S12P21A701-655] feature: Implements VoiceCommandPrompt Component

---
+++ frontend/src/renderer/src/components/common/VoiceCommandPrompt.tsx
@@ -0,0 +1,19 @@
+import propTypes from "prop-types";
+
+interface VoiceCommandPromptProps {
+	text: string;
+}
+
+export const VoiceCommandPrompt: React.FC<VoiceCommandPromptProps> = ({ text }) => {
+	return (
+		<span className="text-6xl inline-flex items-center justify-center px-10 py-5 bg-cloudyBlue rounded-[32px]">
+			{text}
+		</span>
+	);
+};
+
+VoiceCommandPrompt.propTypes = {
+	text: propTypes.string.isRequired
+};
+
+export default VoiceCommandPrompt;
