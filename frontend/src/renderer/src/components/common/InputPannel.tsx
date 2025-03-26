6bb0462c7813777994632a106883100c0539d1cf
이재백 <dbswlrla112@naver.com>
2025-03-26 10:27:24 +0900

    [S12P21A701-667] fix: Adjust InputPanel text to bold text

--- frontend/src/renderer/src/components/common/InputPannel.tsx
+++ frontend/src/renderer/src/components/common/InputPannel.tsx
@@ -18,26 +18,26 @@ export const InputPanel: React.FC<InputPanelProps> = ({
 			<div className="flex flex-col flex-1 justify-between m-10 p-10 bg-white rounded-2xl shadow-custom">
 				<div className="text-right flex flex-col justify-center flex-1">
 					<span className="text-8xl leading-snug">
-						{mainLabel}
+						<span className="font-bold">{mainLabel}</span>
 						{mainLabel === "금액" ? "을" : "를"} 입력하시고
 						<br />
-						<span className="text-green">확인</span>을 눌러주십시오
+						<span className="text-green font-bold">확인</span>을 눌러주십시오
 						{(mainLabel === "비밀번호" || mainLabel === "주민등록번호") && (
 							<>
 								<br />
-								<span className="text-6xl text-red">* 입력하실 때 주의하세요!</span>
+								<span className="text-6xl text-red font-bold">* 입력하실 때 주의하세요!</span>
 							</>
 						)}
 					</span>
 				</div>
 				<div className="flex flex-col p-10 gap-16 bg-background rounded-2xl">
 					<div className="flex justify-between">
-						<span className="text-8xl text-blue">{subLabel}</span>
-						<span className="text-8xl text-blue">1:00</span>
+						<span className="text-8xl text-blue font-bold">{subLabel}</span>
+						<span className="text-8xl text-blue font-bold">1:00</span>
 					</div>
 					<div className="flex flex-col">
 						<input
-							className="flex-1 text-8xl rounded-2xl border-2 border-black text-right px-2.5"
+							className="flex-1 text-8xl rounded-2xl border-2 border-black text-right px-2.5 font-bold"
 							value={format(inputValue)}
 							readOnly
 						/>
