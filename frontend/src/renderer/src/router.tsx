4eb2952fc690095b47ca23745f11e008f8dae56c
이재백 <dbswlrla112@naver.com>
2025-03-26 14:34:55 +0900

    [S12P21A701-697] feature: Add DepositConfirmPage Component in router

--- frontend/src/renderer/src/router.tsx
+++ frontend/src/renderer/src/router.tsx
@@ -1,13 +1,15 @@
 import { createHashRouter } from "react-router-dom";
 import App from "./pages/App";
 import DepositPage from "./pages/deposit/DepositPage";
+import DepositConfirmPage from "./pages/deposit/DepositConfirmPage";
 
 const router = createHashRouter([
 	{
 		path: "/",
 		children: [
 			{ index: true, element: <App /> },
-			{ path: "deposit", element: <DepositPage /> }
+			{ path: "deposit", element: <DepositPage /> },
+			{ path: "deposit/confirm", element: <DepositConfirmPage /> }
 		]
 	}
 ]);
