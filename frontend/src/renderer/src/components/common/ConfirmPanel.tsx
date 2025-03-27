c8c4d629eb32dcf2034d2ec1b092b5e423ce8458
이재백 <dbswlrla112@naver.com>
2025-03-27 16:07:37 +0900

    [S12P21A701-749] feature: Add link props in ConfrimPanel Component

--- frontend/src/renderer/src/components/common/ConfirmPanel.tsx
+++ frontend/src/renderer/src/components/common/ConfirmPanel.tsx
@@ -1,13 +1,15 @@
 import { IoMdClose } from "react-icons/io";
 import { FaRegCircle } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface ConfirmPanelProps {
 	title: string;
 	items: { text: string; value: string }[];
+	link: string;
 }
 
-export const ConfirmPanel = ({ title, items }: ConfirmPanelProps): JSX.Element => {
+export const ConfirmPanel = ({ title, items, link }: ConfirmPanelProps): JSX.Element => {
+	const location = useLocation();
 	return (
 		<div className="flex flex-col h-full justify-between">
 			<div className="m-10 p-10 flex flex-col rounded-2xl bg-white flex-1 justify-between">
@@ -22,13 +24,13 @@ export const ConfirmPanel = ({ title, items }: ConfirmPanelProps): JSX.Element =
 				</div>
 			</div>
 			<div className="h-[20%] grid grid-cols-2 gap-2.5 text-8xl font-bold text-white">
-				<Link to="/" className="block">
+				<Link to={link} className="block">
 					<div className="w-full h-full bg-green inline-flex items-center justify-center gap-8">
 						<FaRegCircle className="size-28" />
 						<span>확인</span>
 					</div>
 				</Link>
-				<Link to="/" className="block">
+				<Link to={location.pathname.includes("specsheet") ? link : "/"} className="block">
 					<div className="w-full h-full bg-red inline-flex items-center justify-center">
 						<IoMdClose className="size-40" />
 						<span>취소</span>
