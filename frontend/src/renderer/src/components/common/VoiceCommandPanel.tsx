a88fbc75f561944b42196aed582282fd5d35346f
이재백 <dbswlrla112@naver.com>
2025-03-27 16:17:32 +0900

    [S12P21A701-757] feature: Add link props in VoiceCommandPanel

--- frontend/src/renderer/src/components/common/VoiceCommandPanel.tsx
+++ frontend/src/renderer/src/components/common/VoiceCommandPanel.tsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 
 interface VoiceCommandPanelProps {
 	title: string;
-	prompts: string[];
+	prompts: { prompt: string; link: string }[];
 }
 
 export const VoiceCommandPanel = ({ title, prompts }: VoiceCommandPanelProps): JSX.Element => {
@@ -21,24 +21,13 @@ export const VoiceCommandPanel = ({ title, prompts }: VoiceCommandPanelProps): J
 					{restText}
 				</span>
 
-				{prompts.map((text, idx) => (
-					<VoiceCommandPrompt key={`${idx}. ${text}`} text={text} />
+				{prompts.map((item, idx) => (
+					<Link to={item.link} key={`${idx}. ${item.prompt}`}>
+						<VoiceCommandPrompt text={item.prompt} />
+					</Link>
 				))}
 
 				<span className="text-4xl text-[#777777]">안전한 금융거래를 위해 음성이 녹음됩니다.</span>
-			</div>
-
-			<div className="flex justify-center gap-5">
-				<Link to={"/deposit"}>
-					<button type="button" className="text-8xl bg-blue rounded-3xl text-white p-5">
-						입금하기
-					</button>
-				</Link>
-				<Link to={"/deposit/confirm"}>
-					<button type="button" className="text-8xl bg-blue rounded-3xl text-white p-5">
-						내용맞음?
-					</button>
-				</Link>
 			</div>
 
 			<VoiceButton />
--- frontend/src/renderer/src/pages/App.tsx
+++ frontend/src/renderer/src/pages/App.tsx
@@ -2,16 +2,19 @@ import AICanvas from "@renderer/components/banker/AICanvas";
 import VoiceCommandPanel from "@renderer/components/common/VoiceCommandPanel";
 
 function App(): JSX.Element {
+	const prompts = [
+		{ prompt: "돈을 넣고 싶어", link: "/deposit" },
+		{ prompt: "돈을 꺼내고 싶어", link: "/withdrawal" },
+		{ prompt: "내가 손녀한테 돈을 보내고 싶어", link: "/transfer" }
+	];
+
 	return (
 		<div className="flex h-full">
 			<div className="h-full" style={{ width: "33.33vw" }}>
 				<AICanvas />
 			</div>
 			<div className="h-full" style={{ width: "66.67vw" }}>
-				<VoiceCommandPanel
-					title="원하시는 업무를 말씀해 주세요"
-					prompts={["돈을 넣고 싶어", "돈을 꺼내고 싶어", "내가 손녀한테 돈을 보내고 싶어"]}
-				/>
+				<VoiceCommandPanel title="원하시는 업무를 말씀해 주세요" prompts={prompts} />
 			</div>
 		</div>
 	);
