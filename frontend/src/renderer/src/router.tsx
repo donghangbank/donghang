d265776fa3337117f83c55e0f5382138873f1444
이재백 <dbswlrla112@naver.com>
2025-03-27 16:11:02 +0900

    [S12P21A701-753] feature: Add deposit paths in router

--- frontend/src/renderer/src/router.tsx
+++ frontend/src/renderer/src/router.tsx
@@ -3,15 +3,49 @@ import App from "./pages/App";
 import DepositPage from "./pages/deposit/DepositPage";
 import DepositConfirmPage from "./pages/deposit/DepositConfirmPage";
 import MainLayout from "./layouts/MainLayout";
+import DepositAccountPage from "./pages/deposit/DepositAccountPage";
+import { NotFoundPage } from "./pages/common/NotFoundPage";
+import DepositPasswordPage from "./pages/deposit/DepositPasswordPage";
+import DepositOptionPage from "./pages/deposit/DepositOptionPage";
+import { DepositAuthPage } from "./pages/deposit/DepositAuthPage";
+import DepositCashInputPage from "./pages/deposit/DepositCashInputPage";
+import DepositCashCountingPage from "./pages/deposit/DepositCashCountingPage";
+import DepositSpecSheetPage from "./pages/deposit/DepositSpecSheetPage";
+import FinalPage from "./pages/common/FinalPage";
 
 const router = createHashRouter([
 	{
 		path: "/",
 		element: <MainLayout />,
+		errorElement: <NotFoundPage />,
 		children: [
 			{ index: true, element: <App /> },
-			{ path: "deposit", element: <DepositPage /> },
-			{ path: "deposit/confirm", element: <DepositConfirmPage /> }
+			{
+				path: "deposit",
+				children: [
+					{ index: true, element: <DepositPage /> },
+					{ path: "account", element: <DepositAccountPage /> },
+					{ path: "confirm", element: <DepositConfirmPage /> },
+					{ path: "password", element: <DepositPasswordPage /> },
+					{ path: "option", element: <DepositOptionPage /> },
+					{ path: "auth", element: <DepositAuthPage /> },
+					{ path: "specsheet", element: <DepositSpecSheetPage /> },
+					{
+						path: "cash",
+						children: [
+							{
+								path: "input",
+								element: <DepositCashInputPage />
+							},
+							{
+								path: "counting",
+								element: <DepositCashCountingPage />
+							}
+						]
+					}
+				]
+			},
+			{ path: "final", element: <FinalPage /> }
 		]
 	}
 ]);
