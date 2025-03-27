cfbf9cd1e2439cb18c54ba1d160440087b3148db
최준혁 <raonrabbit@gmail.com>
2025-03-27 18:08:25 +0900

    [S12P21A701-765] feature: Implement VAD system

--- frontend/src/renderer/src/hooks/websocket/socketMsg.ts
+++ frontend/src/renderer/src/hooks/websocket/socketMsg.ts
@@ -2,9 +2,11 @@ export interface responseMsg {
 	predicted_age?: number;
 	calling_detection?: number;
 	error?: string;
+	user_text?: string;
+	predicted_action?: string;
 }
 
 export interface requestMsg {
 	image?: string;
-	audio?: Blob;
+	audio?: ArrayBuffer;
 }
