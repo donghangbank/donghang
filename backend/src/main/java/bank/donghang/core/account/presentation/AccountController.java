5b8b70517112900244983d3e15436471bbfc99ba
이주현 <cmh1199@naver.com>
2025-04-01 22:15:33 +0900

    [S12P21A701-848] style: Check code conventions

--- backend/src/main/java/bank/donghang/core/account/presentation/AccountController.java
+++ backend/src/main/java/bank/donghang/core/account/presentation/AccountController.java
@@ -2,6 +2,14 @@ package bank.donghang.core.account.presentation;
 
 import jakarta.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import bank.donghang.core.account.application.AccountService;
 import bank.donghang.core.account.dto.request.AccountOwnerNameRequest;
 import bank.donghang.core.account.dto.request.BalanceRequest;
@@ -15,14 +23,6 @@ import bank.donghang.core.account.dto.response.AccountRegisterResponse;
 import bank.donghang.core.account.dto.response.AccountSummaryResponse;
 import bank.donghang.core.account.dto.response.BalanceResponse;
 import bank.donghang.core.common.dto.PageInfo;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
