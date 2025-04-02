5b86cb3e243b596dcefbae19b2de11cda98a2e7b
이재백 <dbswlrla112@naver.com>
2025-04-02 09:50:34 +0900

    [S12P21A701-880] refactor: Update NumberButton Common Component

--- frontend/src/renderer/src/components/common/NumberButton.tsx
+++ frontend/src/renderer/src/components/common/NumberButton.tsx
@@ -1,34 +1,27 @@
-import PropTypes from "prop-types";
-
 interface NumberButtonProps {
 	text: string;
 	bgColor: string;
 	isSquare: boolean;
 	onClick?: () => void;
+	textColor?: string;
 }
 
 export const NumberButton = ({
 	text,
 	bgColor,
 	isSquare,
-	onClick
+	onClick,
+	textColor = "white"
 }: NumberButtonProps): JSX.Element => {
 	return (
 		<button
 			type="button"
-			className={`text-7xl font-bold text-white w-full ${isSquare ? "aspect-square" : "h-full"} rounded-2xl ${bgColor}`}
+			className={`text-${textColor} w-full ${isSquare ? "aspect-square" : "h-full"} rounded-xl ${bgColor}`}
 			onClick={onClick}
 		>
 			{text}
 		</button>
 	);
-};
-
-NumberButton.propTypes = {
-	text: PropTypes.string.isRequired,
-	bgColor: PropTypes.string.isRequired,
-	isSquare: PropTypes.bool.isRequired,
-	onClick: PropTypes.func
 };
 
 export default NumberButton;
