bd40d62bbec7b19210ce38572651ca82fd2c6f91
조강민 <jkm96529@naver.com>
2025-04-02 10:58:16 +0900

    [S12P21A701-894] style: Improve ocr.py code formatting

--- ai/routers/ocr.py
+++ ai/routers/ocr.py
@@ -3,13 +3,13 @@ from utils.ocr_utils import decode_image, extract_name_id
 
 router = APIRouter()
 
+
 @router.post("/ocr")
 async def ocr(file: UploadFile = File(...)):
 
     image = await decode_image(file)
 
-    name, id= extract_name_id(image)
-    
+    name, id = extract_name_id(image)
     if not name or not id:
         raise HTTPException(status_code=404, detail="이름이나 주민등록번호를 찾을 수 없습니다.")
 
