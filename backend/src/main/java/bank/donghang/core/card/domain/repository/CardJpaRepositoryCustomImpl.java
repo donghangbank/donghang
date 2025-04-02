ba9dace165f26e9aa0eaff34e9483dd3564ca442
박종하 <jonghaparkdevelop@gmail.com>
2025-04-02 14:29:02 +0900

    [S12P21A701-913] refactor: Change response spec

--- backend/src/main/java/bank/donghang/core/card/domain/repository/CardJpaRepositoryCustomImpl.java
+++ backend/src/main/java/bank/donghang/core/card/domain/repository/CardJpaRepositoryCustomImpl.java
@@ -6,10 +6,10 @@ import static bank.donghang.core.member.domain.QMember.*;
 
 import org.springframework.stereotype.Repository;
 
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import bank.donghang.core.card.dto.response.CardPasswordResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @Repository
@@ -21,19 +21,24 @@ public class CardJpaRepositoryCustomImpl implements CardJpaRepositoryCustom {
 	@Override
 	public CardPasswordResponse checkCardPassword(String cardNumber) {
 		return queryFactory.select(
-						Projections.constructor(
-								CardPasswordResponse.class,
-								card.cardNumber,
-								card.password,
-								account.accountId,
-								member.name
-						))
-				.from(card)
-				.leftJoin(account)
-				.on(card.accountId.eq(account.accountId))
-				.leftJoin(member)
-				.on(card.ownerId.eq(member.id))
-				.where(card.cardNumber.eq(cardNumber))
-				.fetchOne();
+				Projections.constructor(
+					CardPasswordResponse.class,
+					card.cardNumber,
+					card.password,
+					Expressions.stringTemplate(
+						"concat({0}, concat({1}, {2}))",
+						account.accountTypeCode,
+						account.branchCode,
+						account.accountNumber
+					),
+					member.name
+				))
+			.from(card)
+			.leftJoin(account)
+			.on(card.accountId.eq(account.accountId))
+			.leftJoin(member)
+			.on(card.ownerId.eq(member.id))
+			.where(card.cardNumber.eq(cardNumber))
+			.fetchOne();
 	}
 }
