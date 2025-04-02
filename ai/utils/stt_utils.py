36117d783c8c517d9bc90b0f972333d4470ed0bf
조강민 <jkm96529@naver.com>
2025-04-02 17:42:13 +0900

    [S12P21A701-927] feature: Add STT

--- ai/utils/stt_utils.py
+++ ai/utils/stt_utils.py
@@ -1,22 +1,28 @@
 import logging
-import torch
-from faster_whisper import WhisperModel
 import io
 
 logger = logging.getLogger("logger")
 
-gpu_available = torch.cuda.is_available()
-device = "cuda" if gpu_available else "cpu"
 
-stt_model = WhisperModel("small", device=device, compute_type="int8")
-
-
-async def stt(audio_data: bytes):
+def stt(audio_data: bytes, model):
     """STT 변환"""
     try:
         audio_stream = io.BytesIO(audio_data)
-        segments, _ = stt_model.transcribe(audio_stream, language="ko", vad_filter=True)
+        segments, _ = model.transcribe(
+            audio_stream, batch_size=16, language="ko", vad_filter=True
+        )
         return " ".join([segment.text for segment in segments])
     except Exception as e:
         logger.error(f"STT 변환 오류: {e}")
         return ""
+
+
+def stt_from_path(filepath, model):
+    """오디오 파일 경로를 받아서 STT 변환"""
+    try:
+        segments, _ = model.transcribe(
+            filepath, batch_size=16, language="ko", vad_filter=True
+        )
+        return " ".join([segment.text for segment in segments])
+    except Exception as e:
+        raise RuntimeError(f"STT 변환 오류: {e}")
