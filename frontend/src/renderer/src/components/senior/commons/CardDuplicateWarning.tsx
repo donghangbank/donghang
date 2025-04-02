941d6b2c3a6443ea77fbeb11032328cc70fd6178
최준혁 <raonrabbit@gmail.com>
2025-04-02 17:55:10 +0900

    [S12P21A701-938] feature: Implement senior card cloning warning page

---
+++ frontend/src/renderer/src/components/senior/commons/CardDuplicateWarning.tsx
@@ -0,0 +1,49 @@
+import { IoMdClose } from "react-icons/io";
+import { FaRegCircle } from "react-icons/fa6";
+import { Link, useLocation } from "react-router-dom";
+
+interface ConfirmPanelProps {
+	link: string;
+}
+
+export default function CardDuplicateWarning({ link }: ConfirmPanelProps): JSX.Element {
+	const location = useLocation();
+	return (
+		<div className="flex flex-col h-full justify-between">
+			<div className="m-10 p-10 flex flex-col rounded-2xl bg-white flex-1 justify-between">
+				<div className="flex flex-col p-5 gap-5 bg-background rounded-2xl text-7xl font-bold leading-snug text-center">
+					<p>
+						<span className="text-red">불법 카드</span> 복제 관련
+						<br />
+						최근 <span className="text-red">자동화 기기</span>에{" "}
+						<span className="text-red">카드 복제기</span>를 부착하여
+						<br />
+						고객 정보 탈취를 시도한 사례가 있으오니,
+					</p>
+					<p>
+						<span className="text-red">카드 투입부</span>가 아래 화면과 다른 경우에는
+						<br />
+						사용을 중단하시고 <span className="text-red">영업점</span>이나{" "}
+						<span className="text-red">인터폰</span>으로
+						<br />
+						신고하여 주시기 바랍니다.
+					</p>
+				</div>
+			</div>
+			<div className="h-[20%] grid grid-cols-2 gap-2.5 text-8xl font-bold text-white">
+				<Link to={link} className="block">
+					<div className="w-full h-full bg-green inline-flex items-center justify-center gap-8">
+						<FaRegCircle className="size-28" />
+						<span>확인</span>
+					</div>
+				</Link>
+				<Link to={location.pathname.includes("specsheet") ? link : "/"} className="block">
+					<div className="w-full h-full bg-red inline-flex items-center justify-center">
+						<IoMdClose className="size-40" />
+						<span>취소</span>
+					</div>
+				</Link>
+			</div>
+		</div>
+	);
+}
