7e7f044dad763510034ca3d47fa9eb0a002bb63c
이재백 <dbswlrla112@naver.com>
2025-04-04 17:56:45 +0900

    [S12P21A701-1022] feature: Implement DepositCardWarningPage Component

---
+++ frontend/src/renderer/main/src/pages/general/deposit/DepositCardWarning.tsx
@@ -0,0 +1,7 @@
+import CardWarning from "@renderer/components/common/general/CardWarning";
+
+export const DepositCardWarningPage = (): JSX.Element => {
+	return <CardWarning link="/general/deposit/option" />;
+};
+
+export default DepositCardWarningPage;
