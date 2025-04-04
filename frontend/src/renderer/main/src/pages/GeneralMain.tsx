2a935544de908e60ff2d77a07b499253fae755d9
이재백 <dbswlrla112@naver.com>
2025-04-04 17:54:20 +0900

    [S12P21A701-1019] feature: Update GeneralMain Link path

--- frontend/src/renderer/main/src/pages/GeneralMain.tsx
+++ frontend/src/renderer/main/src/pages/GeneralMain.tsx
@@ -7,12 +7,12 @@ export const GeneralMain = (): JSX.Element => {
 				<span className="text-blue">어떤 업무</span>를 도와드릴까요?
 			</span>
 			<div className="grid grid-cols-2 grid-rows-2 gap-10 font-bold text-6xl text-center">
-				<Link to="/general/deposit">
+				<Link to="/general/deposit/warning/scam">
 					<div className="h-[200px] leading-[200px] bg-cloudyBlue shadow-custom rounded-3xl">
 						입금
 					</div>
 				</Link>
-				<Link to="/general/withdrawal">
+				<Link to="/general/withdrawal/warning/scam">
 					<div className="h-[200px] leading-[200px] bg-cloudyBlue shadow-custom rounded-3xl">
 						출금
 					</div>
