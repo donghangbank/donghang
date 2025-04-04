a1c725ab0bf3e778791fbec86f0dfdfe4fce67aa
이재백 <dbswlrla112@naver.com>
2025-04-05 01:27:33 +0900

    [S12P21A701-1048] feature: Implement WithDrawalOptionPage Component

---
+++ frontend/src/renderer/main/src/pages/general/withdrawal/WithDrawalOptionPage.tsx
@@ -0,0 +1,15 @@
+import card from "@renderer/assets/images/card.png";
+import bankbook from "@renderer/assets/images/bankbook.png";
+import numberpad from "@renderer/assets/images/numberpad.png";
+import Option from "@renderer/components/common/general/Option";
+
+export const WithDrawalOptionPage = (): JSX.Element => {
+	const prompts = [
+		{ prompt: "카드", link: "/general/withdrawal/card/input", imageUrl: card },
+		{ prompt: "통장", link: "/general/withdrawal/card/input", imageUrl: bankbook },
+		{ prompt: "계좌번호", link: "/general/withdrawal/card/input", imageUrl: numberpad }
+	];
+	return <Option prompts={prompts} title="무엇을 가져오셨나요?" />;
+};
+
+export default WithDrawalOptionPage;
