5c484c7c42042c2ebce26536ec4a7b61ffcd9b25
이재백 <dbswlrla112@naver.com>
2025-04-04 17:57:01 +0900

    [S12P21A701-1023] feature: Implement DepositCashCountingPage Component

--- frontend/src/renderer/main/src/pages/general/deposit/DepositCashCountingPage.tsx
+++ frontend/src/renderer/main/src/pages/general/deposit/DepositCashCountingPage.tsx
@@ -1,25 +1,7 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import CashCounting from "@renderer/components/common/general/CashCounting";
 
 export const DepositCashCountingPage = (): JSX.Element => {
-	const navigate = useNavigate();
-
-	useEffect(() => {
-		setTimeout(() => {
-			navigate("/general/deposit/confirm");
-		}, 1000);
-	}, [navigate]);
-
-	return (
-		<div className="flex flex-col h-full  p-10">
-			<div className="flex-1 flex flex-col items-center justify-center w-full bg-white p-10 rounded-2xl shadow-custom">
-				<span className=" text-8xl font-bold text-center leading-snug">
-					<span className="text-red">현금</span>을 세고 있습니다
-					<br /> 잠시만 기다려주십시오
-				</span>
-			</div>
-		</div>
-	);
+	return <CashCounting link="/general/deposit/confirm" />;
 };
 
 export default DepositCashCountingPage;
