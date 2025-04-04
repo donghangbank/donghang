2b86212571d517da8d0c5736f2051c04acd478b8
최준혁 <raonrabbit@gmail.com>
2025-04-04 17:28:40 +0900

    [S12P21A701-1008] feature: Add inputLink mapping for sub screen number pad display

--- frontend/src/renderer/main/src/config/inputLinkMapping.ts
+++ frontend/src/renderer/main/src/config/inputLinkMapping.ts
@@ -1,7 +1,10 @@
 export const inputLinkMapping: Record<string, string> = {
 	"/general/transfer/card/password": "/general/transfer/info/account",
 	"/general/transfer/info/account": "/general/transfer/info/amount",
-	"/general/transfer/info/amount": "/general/transfer/info/specsheet"
+	"/general/transfer/info/amount": "/general/transfer/info/specsheet",
+	"/senior/transfer/card/password": "/senior/transfer/info/account",
+	"/senior/transfer/info/account": "/senior/transfer/info/amount",
+	"/senior/transfer/info/amount": "/senior/transfer/info/specsheet"
 };
 
 export default inputLinkMapping;
