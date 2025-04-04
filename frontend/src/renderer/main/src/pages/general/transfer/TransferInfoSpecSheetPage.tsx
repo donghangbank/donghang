83e22756c9a9adbb4211b7f54b7e934dc788fdd3
이재백 <dbswlrla112@naver.com>
2025-04-04 09:55:44 +0900

    [S12P21A701-980] fix: Change specsheet component styles

--- frontend/src/renderer/main/src/pages/general/transfer/TransferInfoSpecSheetPage.tsx
+++ frontend/src/renderer/main/src/pages/general/transfer/TransferInfoSpecSheetPage.tsx
@@ -16,31 +16,31 @@ export const TransferInfoSpecSheetPage = (): JSX.Element => {
 			<span className="text-5xl font-bold text-center">거래 명세표</span>
 			<div className="flex justify-between gap-20 items-center">
 				<span className="text-blue text-3xl font-bold">거래 일시</span>
-				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[350px]">
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
 					<span>{formatTransactionTime(transactionTime)}</span>
 				</div>
 			</div>
 			<div className="flex justify-between gap-20 items-center">
 				<span className="text-blue text-3xl font-bold">수취인</span>
-				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[350px]">
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
 					<span>{recipientName}</span>
 				</div>
 			</div>
 			<div className="flex justify-between gap-20 items-center">
 				<span className="text-blue text-3xl font-bold">수취계좌</span>
-				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[350px]">
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
 					<span>{formatAccountNumber(receivingAccountNumber)}</span>
 				</div>
 			</div>
 			<div className="flex justify-between gap-20 items-center">
 				<span className="text-blue text-3xl font-bold">금액</span>
-				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[350px]">
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
 					<span>{formatAmount(String(amount))}</span>
 				</div>
 			</div>
 			<div className="flex justify-between gap-20 items-center">
 				<span className="text-blue text-3xl font-bold">잔액</span>
-				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[350px]">
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
 					<span>{formatAmount(String(sendingAccountBalance))}</span>
 				</div>
 			</div>
