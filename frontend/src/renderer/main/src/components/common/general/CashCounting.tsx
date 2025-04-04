4bc7a3d5c538305126ce5530f79c2c80841223e3
이재백 <dbswlrla112@naver.com>
2025-04-04 17:51:03 +0900

    [S12P21A701-1013] feature: Implement cash counting common component

---
+++ frontend/src/renderer/main/src/components/common/general/CashCounting.tsx
@@ -0,0 +1,31 @@
+import { InputContext } from "@renderer/contexts/InputContext";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface CashCountingProps {
+	link: string;
+}
+
+export const CashCounting = ({ link }: CashCountingProps): JSX.Element => {
+	const navigate = useNavigate();
+
+	const { setAmount } = useContext(InputContext);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setAmount("50000");
+			navigate(link);
+		}, 3000);
+	}, [navigate, link, setAmount]);
+
+	return (
+		<div className="flex flex-col bg-white rounded-3xl px-24 py-10 shadow-custom gap-10">
+			<span className="text-6xl font-bold text-center leading-snug">
+				<span className="text-red">현금</span>을 세고 있어요
+				<br /> 잠시만 기다려주세요
+			</span>
+		</div>
+	);
+};
+
+export default CashCounting;
