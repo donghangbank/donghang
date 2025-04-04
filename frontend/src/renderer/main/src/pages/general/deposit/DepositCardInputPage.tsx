f1258cc46e7f8b63021c5b0af5fc13a35d364ff6
이재백 <dbswlrla112@naver.com>
2025-04-04 17:55:28 +0900

    [S12P21A701-1021] feature: Implement DepositCardInputPage Component

---
+++ frontend/src/renderer/main/src/pages/general/deposit/DepositCardInputPage.tsx
@@ -0,0 +1,7 @@
+import CardInput from "@renderer/components/common/general/CardInput";
+
+export const DepositCardInputPage = (): JSX.Element => {
+	return <CardInput link="/general/deposit/card/auth" />;
+};
+
+export default DepositCardInputPage;
