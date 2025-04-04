d011c9fd8c4a5d8acd1da6dc0accaf4fff12015a
이재백 <dbswlrla112@naver.com>
2025-04-05 01:27:19 +0900

    [S12P21A701-1045] feature: Implement WithDrawalCardWarningPage Component

---
+++ frontend/src/renderer/main/src/pages/general/withdrawal/WithDrawalCardWarningPage.tsx
@@ -0,0 +1,7 @@
+import CardWarning from "@renderer/components/common/general/CardWarning";
+
+export const WithDrawalCardWarningPage = (): JSX.Element => {
+	return <CardWarning link="/general/withdrawal/option" />;
+};
+
+export default WithDrawalCardWarningPage;
