bf01391bda8ca151a26d0c97b1d56c70e41c648a
이재백 <dbswlrla112@naver.com>
2025-04-05 14:21:30 +0900

    [S12P21A701-1060] feature: Implement InquiryBalanceCardInputPage Component

---
+++ frontend/src/renderer/main/src/pages/general/inquiry/balance/InquiryBalanceCardInputPage.tsx
@@ -0,0 +1,7 @@
+import CardInput from "@renderer/components/common/general/CardInput";
+
+export const InquiryBalanceCardInputPage = (): JSX.Element => {
+	return <CardInput link="/general/balance/card/auth" />;
+};
+
+export default InquiryBalanceCardInputPage;
