8ffed01658eab8a49ef6004e0f294a272b57d6b3
이재백 <dbswlrla112@naver.com>
2025-04-05 21:34:28 +0900

    [S12P21A701-1080] feature: Implement InquiryHistoryCardWarningPage Component

---
+++ frontend/src/renderer/main/src/pages/general/inquiry/history/InquiryHistoryCardWarningPage.tsx
@@ -0,0 +1,7 @@
+import CardWarning from "@renderer/components/common/general/CardWarning";
+
+export const InquiryHistoryCardWarningPage = (): JSX.Element => {
+	return <CardWarning link="/general/history/option" />;
+};
+
+export default InquiryHistoryCardWarningPage;
