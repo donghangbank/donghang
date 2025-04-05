92d8b804f4e2714286738d01fbf6964ea823fa8c
이재백 <dbswlrla112@naver.com>
2025-04-05 21:34:15 +0900

    [S12P21A701-1077] feature: Implement InquiryHistoryCardAuthPage Component

---
+++ frontend/src/renderer/main/src/pages/general/inquiry/history/InquiryHistoryCardAuthPage.tsx
@@ -0,0 +1,7 @@
+import CardAuth from "@renderer/components/common/general/CardAuth";
+
+export const InquiryHistoryCardAuthPage = (): JSX.Element => {
+	return <CardAuth link="/general/history/card/password" />;
+};
+
+export default InquiryHistoryCardAuthPage;
