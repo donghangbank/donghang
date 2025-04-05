4838130d7f49f7cdd9c859a984b75a7563390acb
이재백 <dbswlrla112@naver.com>
2025-04-05 14:21:26 +0900

    [S12P21A701-1059] feature: Implement InquiryBalanceCardAuthPage Component

---
+++ frontend/src/renderer/main/src/pages/general/inquiry/balance/InquiryBalanceCardAuthPage.tsx
@@ -0,0 +1,7 @@
+import CardAuth from "@renderer/components/common/general/CardAuth";
+
+export const InquiryBalanceCardAuthPage = (): JSX.Element => {
+	return <CardAuth link="/general/balance/card/password" />;
+};
+
+export default InquiryBalanceCardAuthPage;
