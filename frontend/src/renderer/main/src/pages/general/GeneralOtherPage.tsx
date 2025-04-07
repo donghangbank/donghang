ed6ffd94637ca6729b2193800578b08cbbb7a56a
이재백 <dbswlrla112@naver.com>
2025-04-07 17:08:23 +0900

    [S12P21A701-1131] feature: Update Deposit Subscription Route in GeneralOtherPage

--- frontend/src/renderer/main/src/pages/general/GeneralOtherPage.tsx
+++ frontend/src/renderer/main/src/pages/general/GeneralOtherPage.tsx
@@ -3,7 +3,7 @@ import Menu from "@renderer/components/common/general/Menu";
 export const GeneralOtherPage = (): JSX.Element => {
 	const prompts = [
 		{ prompt: "조회", link: "/general/inquiry/option" },
-		{ prompt: "예금 가입", link: "/general" },
+		{ prompt: "예금 가입", link: "/general/products/deposit" },
 		{ prompt: "적금 가입", link: "/general" },
 		{ prompt: "이전", link: "/general" }
 	];
