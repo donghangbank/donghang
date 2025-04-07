27b7a2dfcf427ab07c9f7a7b9838b13ac6ee883a
raonrabbit <raonrabbit@gmail.com>
2025-04-08 05:20:11 +0900

    [S12P21A701-1172] feature: Create senior deposit account and amount selection page component

---
+++ frontend/src/renderer/main/src/pages/senior/products/deposit/SeniorDepositProductInfoAmountPage.tsx
@@ -0,0 +1,12 @@
+import TestButton from "@renderer/components/common/senior/TestButton";
+
+export default function SeniorDepositProductInfoAmountPage(): JSX.Element {
+	return (
+		<>
+			<TestButton
+				prevRoute="/senior/depositproducts/card/password"
+				nextRoute="/senior/depositproducts/info/specsheet"
+			/>
+		</>
+	);
+}
