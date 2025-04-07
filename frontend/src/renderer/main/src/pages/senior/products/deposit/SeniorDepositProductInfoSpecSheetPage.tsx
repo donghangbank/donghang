15d0152405f735a20f3e0e67d095445ac48847ca
raonrabbit <raonrabbit@gmail.com>
2025-04-08 05:21:05 +0900

    [S12P21A701-1173] feature: Create senior deposit spec sheet page component

---
+++ frontend/src/renderer/main/src/pages/senior/products/deposit/SeniorDepositProductInfoSpecSheetPage.tsx
@@ -0,0 +1,9 @@
+import TestButton from "@renderer/components/common/senior/TestButton";
+
+export default function SeniorDepositProductInfoSpecSheetPage(): JSX.Element {
+	return (
+		<>
+			<TestButton prevRoute="/senior/depositproducts/info/amount" nextRoute="/senior/final" />
+		</>
+	);
+}
