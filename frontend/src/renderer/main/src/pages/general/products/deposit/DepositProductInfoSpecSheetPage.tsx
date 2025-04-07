6ac8a92e9cd82424d779115188c0ef6145cab1b2
이재백 <dbswlrla112@naver.com>
2025-04-07 17:09:02 +0900

    [S12P21A701-1138] feature: Implement Completion Page for Deposit Product Subscription

---
+++ frontend/src/renderer/main/src/pages/general/products/deposit/DepositProductInfoSpecSheetPage.tsx
@@ -0,0 +1,54 @@
+import { ProductContext } from "@renderer/contexts/ProductContext";
+import { formatAccountNumber, formatAmount } from "@renderer/utils/formatters";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+export const DepositProductInfoSpecSheetPage = (): JSX.Element => {
+	const { productName, accountBalance, accountNumber, interestRate, accountExpiryDate } =
+		useContext(ProductContext);
+
+	return (
+		<div className="flex flex-col gap-6 bg-white p-10 rounded-3xl shadow-custom">
+			<span className="text-5xl font-bold text-center">가입을 완료했어요</span>
+			<div className="flex justify-between gap-20 items-center">
+				<span className="text-blue text-3xl font-bold">상품명</span>
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
+					<span>{productName}</span>
+				</div>
+			</div>
+			<div className="flex justify-between gap-20 items-center">
+				<span className="text-blue text-3xl font-bold">예치금</span>
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
+					<span>{formatAmount(String(accountBalance))}</span>
+				</div>
+			</div>
+			<div className="flex justify-between gap-20 items-center">
+				<span className="text-blue text-3xl font-bold">계좌번호</span>
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
+					<span>{formatAccountNumber(accountNumber)}</span>
+				</div>
+			</div>
+			<div className="flex justify-between gap-20 items-center">
+				<span className="text-blue text-3xl font-bold">만기일</span>
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
+					<span>{accountExpiryDate}</span>
+				</div>
+			</div>
+			<div className="flex justify-between gap-20 items-center">
+				<span className="text-blue text-3xl font-bold">연 금리</span>
+				<div className="bg-cloudyBlue text-3xl p-5 text-right rounded-3xl font-bold w-[510px]">
+					<span>{interestRate} %</span>
+				</div>
+			</div>
+			<div className="flex justify-center items-center">
+				<button type="button" className="p-6 bg-blue rounded-xl w-full">
+					<Link to={"/general/final"}>
+						<span className="text-3xl text-white">확인</span>
+					</Link>
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default DepositProductInfoSpecSheetPage;
