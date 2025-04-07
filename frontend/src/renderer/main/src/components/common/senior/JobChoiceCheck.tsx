23cc57d86b059001052069bbad88f1e76e029533
최준혁 <raonrabbit@gmail.com>
2025-04-07 17:34:35 +0900

    [S12P21A701-1144] refactor: Extract initial confirmation page

---
+++ frontend/src/renderer/main/src/components/common/senior/JobChoiceCheck.tsx
@@ -0,0 +1,45 @@
+import { useActionPlay } from "@renderer/hooks/ai/useActionPlay";
+import { useContext, useEffect } from "react";
+import { AIContext } from "@renderer/contexts/AIContext";
+import { useNavigate } from "react-router-dom";
+import TestButton from "@renderer/components/common/senior/TestButton";
+import { PageContext } from "@renderer/contexts/PageContext";
+
+interface JobCheckProps {
+	job: string;
+	audioFile?: string;
+	prev: string;
+	link: string;
+}
+
+export default function JobChoiceCheck({ job, audioFile, prev, link }: JobCheckProps): JSX.Element {
+	const { construction } = useContext(AIContext);
+	const { setCurrentJob } = useContext(PageContext);
+	const navigate = useNavigate();
+
+	useActionPlay({
+		audioFile: audioFile,
+		dialogue: `${job} 도와드리면 될까요?`,
+		shouldActivate: true,
+		avatarState: "idle"
+	});
+
+	useEffect(() => {
+		if (construction === "긍정") {
+			navigate(link);
+			setCurrentJob(job);
+		} else if (construction === "부정" || construction === "홈") {
+			navigate("/");
+		}
+	}, [construction, navigate, setCurrentJob, job, link]);
+
+	useEffect(() => {
+		setCurrentJob(job);
+	}, [setCurrentJob, job]);
+
+	return (
+		<>
+			<TestButton prevRoute={prev} nextRoute={link} />
+		</>
+	);
+}
