8d4985e4aced64d85bc13446249f5ec8f9baf73d
이재백 <dbswlrla112@naver.com>
2025-04-07 17:08:04 +0900

    [S12P21A701-1127] feature: Add ProductProvider to AppProvider

--- frontend/src/renderer/main/src/contexts/AppProvider.tsx
+++ frontend/src/renderer/main/src/contexts/AppProvider.tsx
@@ -1,17 +1,19 @@
-import React from "react";
 import { AIProvider } from "./AIProvider";
 import { UserProvider } from "./UserProvider";
 import { InputProvider } from "./InputProvider";
 import { PageProvider } from "./PageProvider";
 import { SpecSheetProvider } from "./SpecSheetProvider";
+import { ProductProvider } from "./ProductProvider";
 
-export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const AppProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
 	return (
 		<AIProvider>
 			<UserProvider>
 				<InputProvider>
 					<SpecSheetProvider>
-						<PageProvider>{children}</PageProvider>
+						<ProductProvider>
+							<PageProvider>{children}</PageProvider>
+						</ProductProvider>
 					</SpecSheetProvider>
 				</InputProvider>
 			</UserProvider>
