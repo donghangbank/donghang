bf91d6d9299e195b9f301f9fbd97dc0cbc79c3b6
이재백 <dbswlrla112@naver.com>
2025-04-07 17:08:37 +0900

    [S12P21A701-1134] feature: Implement Card Insertion Page for Deposit Subscription

---
+++ frontend/src/renderer/main/src/pages/general/products/deposit/DepositProductCardInputPage.tsx
@@ -0,0 +1,7 @@
+import CardInput from "@renderer/components/common/general/CardInput";
+
+export const DepositProductCardInputPage = (): JSX.Element => {
+	return <CardInput link="/general/products/deposit/card/auth" />;
+};
+
+export default DepositProductCardInputPage;
