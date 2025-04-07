882391d97e555cebee3997cc063e01060dc7b059
이재백 <dbswlrla112@naver.com>
2025-04-07 10:27:13 +0900

    [S12P21A701-1100] feature: Adjust GeneralMain Component

--- frontend/src/renderer/main/src/pages/GeneralMain.tsx
+++ frontend/src/renderer/main/src/pages/GeneralMain.tsx
@@ -1,35 +1,14 @@
-import { Link } from "react-router-dom";
+import Menu from "@renderer/components/common/general/Menu";
 
 export const GeneralMain = (): JSX.Element => {
-	return (
-		<div className="flex flex-col bg-white rounded-3xl p-10 shadow-custom gap-10">
-			<span className="text-6xl font-bold text-center">
-				<span className="text-blue">어떤 업무</span>를 도와드릴까요?
-			</span>
-			<div className="grid grid-cols-2 grid-rows-2 gap-10 font-bold text-6xl text-center">
-				<Link to="/general/deposit/warning/scam">
-					<div className="h-[200px] leading-[200px] bg-cloudyBlue shadow-custom rounded-3xl">
-						입금
-					</div>
-				</Link>
-				<Link to="/general/withdrawal/warning/scam">
-					<div className="h-[200px] leading-[200px] bg-cloudyBlue shadow-custom rounded-3xl">
-						출금
-					</div>
-				</Link>
-				<Link to="/general/transfer/warning/scam">
-					<div className="h-[200px] leading-[200px] bg-cloudyBlue shadow-custom rounded-3xl">
-						이체
-					</div>
-				</Link>
-				<Link to="/general/inquiry/option">
-					<div className="h-[200px] leading-[200px] bg-cloudyBlue shadow-custom rounded-3xl">
-						조회
-					</div>
-				</Link>
-			</div>
-		</div>
-	);
+	const prompts = [
+		{ prompt: "입금", link: "/general/deposit/warning/scam" },
+		{ prompt: "출금", link: "/general/withdrawal/warning/scam" },
+		{ prompt: "이체", link: "/general/transfer/warning/scam" },
+		{ prompt: "기타", link: "/general/others" }
+	];
+
+	return <Menu prompts={prompts} />;
 };
 
 export default GeneralMain;
