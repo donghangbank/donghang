5e0cf5f8736ec01c36cb6fc1e3f329304bad5f8a
조강민 <jkm96529@naver.com>
2025-04-07 13:36:33 +0900

    [S12P21A701-1113] feature: Implement account recommendation router

--- ai/main.py
+++ ai/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from routers import ocr, video_ws, audio_ws, predict_action
+from routers import ocr, video_ws, audio_ws, predict_action, recommend_account
 
 app = FastAPI()
 
@@ -19,6 +19,7 @@ app.include_router(ocr.router)
 app.include_router(video_ws.router)
 app.include_router(audio_ws.router)
 app.include_router(predict_action.router)
+app.include_router(recommend_account.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
