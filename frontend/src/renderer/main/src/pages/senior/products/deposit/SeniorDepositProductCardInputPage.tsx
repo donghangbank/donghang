9aa2af323c014af56bb2fb3ec2abdd72fd4b197d
raonrabbit <raonrabbit@gmail.com>
2025-04-08 05:17:24 +0900

    [S12P21A701-1169] feature: Implement senior deposit card input page component

---
+++ frontend/src/renderer/main/src/pages/senior/products/deposit/SeniorDepositProductCardInputPage.tsx
@@ -0,0 +1,7 @@
+import CardInput from "@renderer/components/common/senior/CardInput";
+
+export default function SeniorDepositProductCardInputPage(): JSX.Element {
+	return (
+		<CardInput prev="/senior/depositproducts/option" link="/senior/depositproducts/card/auth" />
+	);
+}
