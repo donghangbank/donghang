97d709b669ae67e0c44f71758bdd808270b232f9
이재백 <dbswlrla112@naver.com>
2025-04-08 12:42:06 +0900

    [S12P21A701-1191] feature: Implement installment product registration page for card insertion

---
+++ frontend/src/renderer/main/src/pages/general/products/installment/InstallmentProductCardInputPage.tsx
@@ -0,0 +1,7 @@
+import CardInput from "@renderer/components/common/general/CardInput";
+
+export const InstallmentProductCardInputPage = (): JSX.Element => {
+	return <CardInput link="/general/installmentproducts/card/auth" />;
+};
+
+export default InstallmentProductCardInputPage;
