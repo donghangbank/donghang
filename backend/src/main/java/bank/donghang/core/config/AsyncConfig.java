553015c6aaf05073d3b09c694c4c0cc010c44cce
박종하 <jonghaparkdevelop@gmail.com>
2025-04-08 11:06:57 +0900

    [S12P21A701-1178] feature: Configure AsyncConfig and Define LedgerExecutor

---
+++ backend/src/main/java/bank/donghang/core/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package bank.donghang.core.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+	@Bean(name = "ledgerExecutor")
+	public Executor ledgerTaskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("LedgerAsync-");
+		executor.initialize();
+		return executor;
+	}
+}
