3d6b4f6cdb7f34ff904888883fcad4e00e5c23a6
이재백 <dbswlrla112@naver.com>
2025-04-08 12:43:06 +0900

    [S12P21A701-1199] feature: Implement interfaces related to installment product registration

--- frontend/src/renderer/main/src/types/products.ts
+++ frontend/src/renderer/main/src/types/products.ts
@@ -19,10 +19,35 @@ export interface registerDepositProductAPIResponse {
 	accountBalance: number;
 	interestRate: number;
 	accountExpiryDate: string;
+	nextInstallmentScheduleDate?: string;
 }
 
 export interface getDepositProductsAPIResponse {
 	pageToken: number | null;
 	data: DepositProduct[];
+	hasNext: boolean;
+}
+
+export interface InstallmentProduct {
+	productName?: string;
+	productDescription?: string;
+	accountProductId: number;
+	accountProductName: string;
+	bankId: number;
+	bankName: string;
+	bankLogoUrl: string;
+	interestRate: number;
+	minSubscriptionBalance: number;
+	maxSubscriptionBalance: number;
+	subscriptionPeriod: number;
+	accountProductType: string;
+	productTypeCode?: number;
+	productTypeName: string;
+	rateDescription: string;
+}
+
+export interface getInstallmentProductsAPIResponse {
+	pageToken: number | null;
+	data: InstallmentProduct[];
 	hasNext: boolean;
 }
