edbd0d1128f55a5b7ffd19ad428aaf695a355047
박종하 <jonghaparkdevelop@gmail.com>
2025-04-09 09:51:55 +0900

    [S12P21A701-707] style: Update classes to format style

--- backend/src/main/java/bank/donghang/core/ledger/domain/repository/JournalLineJdbcRepository.java
+++ backend/src/main/java/bank/donghang/core/ledger/domain/repository/JournalLineJdbcRepository.java
@@ -1,14 +1,15 @@
 package bank.donghang.core.ledger.domain.repository;
 
-import bank.donghang.core.ledger.domain.JournalLine;
-import lombok.RequiredArgsConstructor;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
+import bank.donghang.core.ledger.domain.JournalLine;
+import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,26 +18,26 @@ public class JournalLineJdbcRepository {
 	private final JdbcTemplate jdbcTemplate;
 
 	public void batchInsert(List<JournalLine> journalLines) {
-		String sql = "INSERT INTO journal_line " +
-				"(journal_entry_id, account_id, entry_type, amount, created_at, modified_at) " +
-				"VALUES (?, ?, ?, ?, NOW(), NOW())";
+		String sql = "INSERT INTO journal_line "
+			+ "(journal_entry_id, account_id, entry_type, amount, created_at, modified_at) "
+			+ "VALUES (?, ?, ?, ?, NOW(), NOW())";
 
 		jdbcTemplate.batchUpdate(sql,
-				new BatchPreparedStatementSetter() {
+			new BatchPreparedStatementSetter() {
 
-					@Override
-					public void setValues(PreparedStatement ps, int i) throws SQLException {
-						JournalLine journalLine = journalLines.get(i);
-						ps.setLong(1, journalLine.getJournalEntryId());
-						ps.setLong(2, journalLine.getAccountId());
-						ps.setString(3, journalLine.getEntryType().toString());
-						ps.setDouble(4, journalLine.getAmount());
-					}
+				@Override
+				public void setValues(PreparedStatement ps, int idx) throws SQLException {
+					JournalLine journalLine = journalLines.get(idx);
+					ps.setLong(1, journalLine.getJournalEntryId());
+					ps.setLong(2, journalLine.getAccountId());
+					ps.setString(3, journalLine.getEntryType().toString());
+					ps.setDouble(4, journalLine.getAmount());
+				}
 
-					@Override
-					public int getBatchSize() {
-						return journalLines.size();
-					}
-				});
+				@Override
+				public int getBatchSize() {
+					return journalLines.size();
+				}
+			});
 	}
 }
