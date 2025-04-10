24fa5b4a2711f1fa6489a5be3203858d76168281
이재백 <dbswlrla112@naver.com>
2025-04-10 13:44:43 +0900

    [S12P21A701-1253] refactor: Adjust scam warning component text

--- frontend/src/renderer/main/src/components/common/general/ScamWarning.tsx
+++ frontend/src/renderer/main/src/components/common/general/ScamWarning.tsx
@@ -18,9 +18,12 @@ export const ScamWarning = ({ link }: ScamWarningProps): JSX.Element => {
 					최근 <span className="text-red">공공기관</span>
 					{"(국세청, 경찰청, 검찰청, 금감원 등)이나"}{" "}
 					<span className="text-red">금융기관 직원</span>을{" "}
-					<span className="text-red">사칭한 금융사기</span>가 자주 발생하고 있습니다.
+					<span className="text-red">사칭한 금융사기</span>가<br />
+					자주 발생하고 있습니다.
 					<br />
-					현금을 요구받으면 <span className="text-red">100% 보이스피싱</span>입니다.
+					현금을 요구받으면
+					<br />
+					<span className="text-red">100% 보이스피싱</span>입니다.
 				</span>
 			</div>
 			<div className="grid grid-cols-2 gap-5 text-white text-7xl font-bold">
